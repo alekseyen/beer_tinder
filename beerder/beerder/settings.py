371265868938550e05bba779c0435b6eef20e224
pilot7747 <pilot7747@gmail.com>
2019-11-19 23:08:46 +0300

    CORS fix4

--- beerder/beerder/settings.py
+++ beerder/beerder/settings.py
@@ -43,13 +43,6 @@ INSTALLED_APPS = [
     'api'
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8080",
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "http://127.0.0.1:8000"
-]
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
